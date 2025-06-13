09f51d87baecd5ce7d9d7c0de0bf1580ea8bc1ca
RafaelTS <rafaeltorress@gmail.com>
2025-06-13 16:43:41 -0300

    cy files configurated with the cucumbe specifications

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,7 +1,39 @@
 describe('Search Product', () => {
-  describe('Search Product', () => {
-    it('', () => {
+  describe('Submitting an image with invalid inputs', () => {
+
+    const input = {
+      product: 'Huggies Premium Natural Care Diaper M 32 Units'
+    }
+
+    it('Given that I am on the Amazon website', () => {
       cy.visit('/')      
+    })
+    it(`And I search for the product "${input.product}"`, () => {
+      
+    });
+    it(`When I click enter`, () => {
+      
+    })
+    it(`Then the product will be displayed`, () => {
+      
+    })
+    it(`And I add a unit`, () => {
+      
+    })
+    it(`And I go to the shopping cart`, () => {
+      
+    })
+    it(`And the price shown is the same price as when I searched for the product`, () => {
+      
+    })
+    it(`And I add one more product`, () => {
+      
+    })
+    it(`And the quantity has increased to 2`, () => {
+      
+    })
+    it(`And the subtotal value is updated`, () => {
+      
     })
   })
 })
